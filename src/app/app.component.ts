b13e289f306c3b4576334f2a95f7b4e091d8638b
nandini <nandani@contaque.com>
2025-05-08 12:20:43 +0530

    Add the changes
    Signed-off-by: nandini <nandani@contaque.com>

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,4 +9,10 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'forloopcontextvariable12';
+  //contextual variables are those variables jo for loop ke andar avaibale hote hai like ke @for(statement){
+  //
+  //}    isme curly braces ke andar wale varibales contextual variable honge and yeh khud bane hote hai hume khud define karne ke need nhi hote
+  // contextual variables are $count,$index,$last,$first,$odd,$even
+  users=['anil','sam','peter','ram','rohan','raj','mohan','sumit']
+  usev=[]
 }
